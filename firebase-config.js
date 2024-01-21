cfd7d85071ffa52de9b43ef5733c0ef749ea6d4a
SolanaZavr <105708476+Plesh4kov@users.noreply.github.com>
2024-01-21 21:06:32 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Ваши настройки конфигурации Firebase
+const firebaseConfig = {
+  apiKey: "wvTBXZZK8MGbH3kZKTsO8WNIa",
+  authDomain: "qqgARObdfEdiERaphJLyE9MhhPiV3VBNdmQrq8QWBC8dFoppRl",
+  // ... другие настройки Firebase ...
+};
+
+// Инициализация Firebase
+firebase.initializeApp(firebaseConfig);
