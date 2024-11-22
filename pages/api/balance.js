c2b165b15b38150945fa79818231c443651db3c9
SolanaZavr <105708476+Plesh4kov@users.noreply.github.com>
2024-11-22 17:41:50 +0200

    Update balance.js

--- pages/api/balance.js
+++ pages/api/balance.js
@@ -1,12 +1,14 @@
-import axios from "axios";
-
 export default async function handler(req, res) {
   const walletAddress = "3HMkAtzMwjH9mVZwR6rgGL6f2P5z7BswAx4FnqEcubWp"; // Адрес кошелька
   const url = `https://solscan.io/account/${walletAddress}`;
 
   try {
-    const response = await axios.get(url);
-    const html = response.data;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const html = await response.text();
 
     // Регулярное выражение для поиска баланса в HTML-странице
     const balanceRegex = /<div[^>]*class="[^"]*account-balance[^"]*">[\s\S]*?<span[^>]*class="value"[^>]*>([\s\S]*?)<\/span>/;
